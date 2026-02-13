1b8dd3107eca95ea1966b7d99454952b62a5ad36
nanashi <cibicco323@gmail.com>
2026-02-13 16:52:16 +0900

    コード品質改善・不要ファイル削除
    - 未使用マイグレーション削除 (failed_jobs, personal_access_tokens)
    - favoritesの複合ユニーク制約を1つのマイグレーションに統合
    - インラインstyleをCSS classes に移行 (app.blade.php)
    - 未定義CSSクラス削除 (show.blade.php)
    - ItemControllerの条件分岐をフラット化
    - ItemSeederの変数名修正

--- src/database/seeders/ItemSeeder.php
+++ src/database/seeders/ItemSeeder.php
@@ -11,109 +11,108 @@ use Illuminate\Support\Facades\Storage;
 
 class ItemSeeder extends Seeder
 {
+    private const ITEMS = [
+        [
+            'name' => '腕時計',
+            'price' => 15000,
+            'brand' => 'Rolax',
+            'description' => 'スタイリッシュなデザインのメンズ腕時計',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
+            'condition' => '良好',
+            'categories' => ['ファッション', 'メンズ', 'アクセサリー'],
+        ],
+        [
+            'name' => 'HDD',
+            'price' => 5000,
+            'brand' => '西芝',
+            'description' => '高速で信頼性の高いハードディスク',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
+            'condition' => '目立った傷や汚れなし',
+            'categories' => ['家電'],
+        ],
+        [
+            'name' => '玉ねぎ3束',
+            'price' => 300,
+            'brand' => 'なし',
+            'description' => '新鮮な玉ねぎ3束のセット',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/iLoveIMG+d.jpg',
+            'condition' => 'やや傷や汚れあり',
+            'categories' => ['キッチン'],
+        ],
+        [
+            'name' => '革靴',
+            'price' => 4000,
+            'brand' => null,
+            'description' => 'クラシックなデザインの革靴',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Leather+Shoes+Product+Photo.jpg',
+            'condition' => '状態が悪い',
+            'categories' => ['ファッション', 'メンズ'],
+        ],
+        [
+            'name' => 'ノートPC',
+            'price' => 45000,
+            'brand' => null,
+            'description' => '高性能なノートパソコン',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Living+Room+Laptop.jpg',
+            'condition' => '良好',
+            'categories' => ['家電'],
+        ],
+        [
+            'name' => 'マイク',
+            'price' => 8000,
+            'brand' => 'なし',
+            'description' => '高音質のレコーディング用マイク',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Music+Mic+4632231.jpg',
+            'condition' => '目立った傷や汚れなし',
+            'categories' => ['家電'],
+        ],
+        [
+            'name' => 'ショルダーバッグ',
+            'price' => 3500,
+            'brand' => null,
+            'description' => 'おしゃれなショルダーバッグ',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Purse+fashion+pocket.jpg',
+            'condition' => 'やや傷や汚れあり',
+            'categories' => ['ファッション', 'レディース'],
+        ],
+        [
+            'name' => 'タンブラー',
+            'price' => 500,
+            'brand' => 'なし',
+            'description' => '使いやすいタンブラー',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
+            'condition' => '状態が悪い',
+            'categories' => ['キッチン'],
+        ],
+        [
+            'name' => 'コーヒーミル',
+            'price' => 4000,
+            'brand' => 'Starbacks',
+            'description' => '手動のコーヒーミル',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Waitress+with+Coffee+Grinder.jpg',
+            'condition' => '良好',
+            'categories' => ['キッチン'],
+        ],
+        [
+            'name' => 'メイクセット',
+            'price' => 2500,
+            'brand' => null,
+            'description' => '便利なメイクアップセット',
+            'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
+            'condition' => '目立った傷や汚れなし',
+            'categories' => ['コスメ', 'レディース'],
+        ],
+    ];
+
     public function run()
     {
         $user = User::first();
-
         $conditions = Condition::pluck('id', 'name');
         $categories = Category::pluck('id', 'name');
 
         Storage::disk('public')->makeDirectory('items');
 
-        $items = [
-            [
-                'name' => '腕時計',
-                'price' => 15000,
-                'brand' => 'Rolax',
-                'description' => 'スタイリッシュなデザインのメンズ腕時計',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
-                'condition' => '良好',
-                'categories' => ['ファッション', 'メンズ', 'アクセサリー'],
-            ],
-            [
-                'name' => 'HDD',
-                'price' => 5000,
-                'brand' => '西芝',
-                'description' => '高速で信頼性の高いハードディスク',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
-                'condition' => '目立った傷や汚れなし',
-                'categories' => ['家電'],
-            ],
-            [
-                'name' => '玉ねぎ3束',
-                'price' => 300,
-                'brand' => 'なし',
-                'description' => '新鮮な玉ねぎ3束のセット',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/iLoveIMG+d.jpg',
-                'condition' => 'やや傷や汚れあり',
-                'categories' => ['キッチン'],
-            ],
-            [
-                'name' => '革靴',
-                'price' => 4000,
-                'brand' => null,
-                'description' => 'クラシックなデザインの革靴',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Leather+Shoes+Product+Photo.jpg',
-                'condition' => '状態が悪い',
-                'categories' => ['ファッション', 'メンズ'],
-            ],
-            [
-                'name' => 'ノートPC',
-                'price' => 45000,
-                'brand' => null,
-                'description' => '高性能なノートパソコン',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Living+Room+Laptop.jpg',
-                'condition' => '良好',
-                'categories' => ['家電'],
-            ],
-            [
-                'name' => 'マイク',
-                'price' => 8000,
-                'brand' => 'なし',
-                'description' => '高音質のレコーディング用マイク',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Music+Mic+4632231.jpg',
-                'condition' => '目立った傷や汚れなし',
-                'categories' => ['家電'],
-            ],
-            [
-                'name' => 'ショルダーバッグ',
-                'price' => 3500,
-                'brand' => null,
-                'description' => 'おしゃれなショルダーバッグ',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Purse+fashion+pocket.jpg',
-                'condition' => 'やや傷や汚れあり',
-                'categories' => ['ファッション', 'レディース'],
-            ],
-            [
-                'name' => 'タンブラー',
-                'price' => 500,
-                'brand' => 'なし',
-                'description' => '使いやすいタンブラー',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
-                'condition' => '状態が悪い',
-                'categories' => ['キッチン'],
-            ],
-            [
-                'name' => 'コーヒーミル',
-                'price' => 4000,
-                'brand' => 'Starbacks',
-                'description' => '手動のコーヒーミル',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Waitress+with+Coffee+Grinder.jpg',
-                'condition' => '良好',
-                'categories' => ['キッチン'],
-            ],
-            [
-                'name' => 'メイクセット',
-                'price' => 2500,
-                'brand' => null,
-                'description' => '便利なメイクアップセット',
-                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
-                'condition' => '目立った傷や汚れなし',
-                'categories' => ['コスメ', 'レディース'],
-            ],
-        ];
-
-        foreach ($items as $itemData) {
+        foreach (self::ITEMS as $itemData) {
             $imageContents = file_get_contents($itemData['image']);
             $imageName = 'items/' . uniqid() . '.jpg';
             Storage::disk('public')->put($imageName, $imageContents);
